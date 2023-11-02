b8e7b367eb5d788885a85de8a82e14759127478f
cmiranda <candidoguilhermefbm@gmail.com>
2023-11-02 14:57:09 -0300

    find fatorial number using recursion

---
+++ Find Factorial of Number Using Recursion/findfatorial.js
@@ -0,0 +1,13 @@
+function fat(x) {
+    if (x == 1) {
+        return 1
+    }
+    else {
+        return (x * fat(x-1))
+    }
+
+}
+
+console.log(fat(3))
+console.log(fat(5))
+console.log(fat(32))
